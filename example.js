f789e6d1db8169d8c4935f883f6282d4cdb364c4
Vinogradov Victor <re.horoyami.not@gmail.com>
2018-10-23 20:19:02 +0300

    Refactoring: I get that querySelector is better. Code has not changed

--- example.js
+++ example.js
@@ -1,5 +1,5 @@
 import {TableConstructor} from "./src/tableConstructor";
 
-let table = document.getElementsByClassName("main_wrapper")[0];
+let table = document.querySelector(".main_wrapper");
 let table_editor = new TableConstructor({width: "800px"});
 table.appendChild(table_editor.getTableDOM());
