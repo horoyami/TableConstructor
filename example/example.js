1be841b6d1fe4b77fdf9c6418c127b980120f0db
Vinogradov Victor <re.horoyami.not@gmail.com>
2018-10-26 12:01:48 +0300

    ES6 upgrade: documentUtils

--- example/example.js
+++ example/example.js
@@ -1,5 +1,6 @@
 import {TableConstructor} from "../tableConstructor";
 
 let table = document.querySelector(".main_wrapper");
-let table_editor = new TableConstructor({width: "800px"});
+let table_editor = new TableConstructor();
+table_editor.style.width = "800px";
 table.appendChild(table_editor.getTableDOM());
--- src/documentUtils.js
+++ src/documentUtils.js
@@ -1,47 +1,52 @@
 /**
- * The object provides helper methods for working with DOM
+ * Checks unless the item is missing
+ * @param {object|array} elem - elememt
+ * @returns {boolean} true if element contains something
+ * @private
  */
-export const DOM = (function () {
+function _checkIsNotMissing(elem) {
+    return (!(elem === undefined || elem === null));
+}
 
-    /**
-     * Checks if there is any important information in the variable
-     * @param {object|array} elem - elememt
-     * @returns {boolean} true if element contains something
-     * @private
-     */
-    function _checkElementExisting(elem) {
-        return (!(elem === undefined || elem === null));
+/**
+ * Create DOM element with set parameters
+ * @param {string} tagName - Html tag of the element to be created
+ * @param {array} cssClasses - Css classes that must be applied to an element
+ * @param {object} attrs - Attributes that must be applied to the element
+ * @param {array} children - child elements of creating element
+ * @returns {HTMLElement} the new element
+ */
+export function createDOMElement(tagName, cssClasses, attrs, children) {
+    const elem = document.createElement(tagName);
+    if (_checkIsNotMissing(cssClasses)) {
+        for (let i = 0; i < cssClasses.length; i++) {
+            elem.classList.add(cssClasses[i]);
+        }
     }
-
-    /**
-     * Create DOM element with set parameters
-     * @param {string} tagName - Html tag of the element to be created
-     * @param {array} cssClasses - Css classes that must be applied to an element
-     * @param {object} attrs - Attributes that must be applied to the element
-     * @param {array} children - child elements of creating element
-     * @returns {HTMLElement} the new element
-     */
-    function createDOMElement(tagName, cssClasses, attrs, children) {
-        const elem = document.createElement(tagName);
-        if (_checkElementExisting(cssClasses)) {
-            for (let i = 0; i < cssClasses.length; i++) {
-                elem.classList.add(cssClasses[i]);
-            }
+    if (_checkIsNotMissing(attrs)) {
+        for (let key in attrs) {
+            elem.setAttribute(key, attrs[key]);
         }
-        if (_checkElementExisting(attrs)) {
-            for (let key in attrs) {
-                elem.setAttribute(key, attrs[key]);
-            }
-        }
-        if (_checkElementExisting(children)) {
-            for(let i = 0; i < children.length; i++) {
-                elem.appendChild(children[i]);
-            }
-        }
-        return elem;
     }
+    if (_checkIsNotMissing(children)) {
+        for(let i = 0; i < children.length; i++) {
+            elem.appendChild(children[i]);
+        }
+    }
+    return elem;
+}
 
+/**
+ * Get item position relative to document
+ * @param {HTMLElement} elem - item
+ * @returns {object} coordinates of the upper left (x1,y1) and lower right(x2,y2) corners
+ */
+export function getCoords(elem) {
+    const rect = elem.getBoundingClientRect();
     return {
-        createDOMElement: createDOMElement
-    };
-})();
+        y1: rect.top + pageYOffset,
+        x1: rect.left + pageXOffset,
+        x2: rect.right + pageXOffset,
+        y2: rect.bottom + pageYOffset
+    }
+}
