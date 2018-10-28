3b28710f858e59c0034f800fc5cb9378ddd703d1
Vinogradov Victor <re.horoyami.not@gmail.com>
2018-10-28 12:05:51 +0300

    rewrite file again

---
+++ src/tableConstructor.js
@@ -0,0 +1,75 @@
+import "./tableConstructor.css"
+import {createDOMElement, getCoords} from "./documentUtils";
+import {ActivatingAreaAroundContainer} from "./activatingAreaAroundContainer";
+import {HorizontalBorderToolBar, VerticalBorderToolBar} from "./borderToolBar";
+import {Table} from "./table";
+
+const CSS = {
+    editor: "tcm-table-editor"
+};
+
+export class TableConstructor {
+
+    constructor() {
+        this._table = this._createBlankTable();
+        this._container = new ActivatingAreaAroundContainer(this._table.htmlElement, false);
+        this._container.htmlElement.classList.add(CSS.editor);
+        this._verticalToolBar = new VerticalBorderToolBar();
+        this._horizontalToolBar = new HorizontalBorderToolBar();
+        this._container.htmlElement.appendChild(this._verticalToolBar.htmlElement);
+        this._container.htmlElement.appendChild(this._horizontalToolBar.htmlElement);
+        this._coveredBlock = null;
+        this._activatedToolBar = null;
+        this._hangEvents();
+    }
+
+    get htmlElement() {
+        return this._container.htmlElement;
+    }
+
+    _createBlankTable() {
+        const table = new Table();
+        table.addColumn();
+        table.addColumn();
+        table.addRow();
+        table.addRow();
+        return table;
+    }
+
+    _setHoverBlock(content) {
+        this._coveredBlock = content;
+        while (!(this._coveredBlock === null || this._coveredBlock.tagName === "TD" || this._coveredBlock === this._container.htmlElement))
+            this._coveredBlock = this._coveredBlock.parentElement;
+    }
+
+    _showToolBar(toolBar, coord) {
+        if (this._activatedToolBar !== null)
+            this._activatedToolBar.hide();
+        this._activatedToolBar = toolBar;
+        toolBar.showIn(coord);
+    }
+
+    _hangEvents() {
+        this._container.htmlElement.addEventListener("mouseInActivatingArea", (event) => {
+            this._side = event.detail.side;
+            const areaCoords = getCoords(event.target);
+            const containerCoords = getCoords(this._container.htmlElement);
+            this._setHoverBlock(event.target);
+
+            if (this._side === "top") {
+                this._showToolBar(this._horizontalToolBar, areaCoords.y1 - containerCoords.y1);
+            }
+            if (this._side === "bottom") {
+                this._showToolBar(this._horizontalToolBar, areaCoords.y2 - containerCoords.y1);
+            }
+            if (this._side === "left") {
+                this._showToolBar(this._verticalToolBar, areaCoords.x1 - containerCoords.x1 - 1);
+            }
+            if (this._side === "right") {
+                this._showToolBar(this._verticalToolBar, areaCoords.x2 - containerCoords.x1);
+            }
+        });
+    }
+
+
+}
