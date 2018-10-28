9e570a81545f9f301138105a9ab129b1d49b1e0c
Vinogradov Victor <re.horoyami.not@gmail.com>
2018-10-28 14:25:50 +0300

    Lint and JSDoc: table

--- src/table.js
+++ src/table.js
@@ -1,97 +1,150 @@
-import {createDOMElement} from "./documentUtils";
-import {ActivatingAreaAroundContainer} from "./activatingAreaAroundContainer";
-import "./table.css";
+import {createDOMElement} from './documentUtils';
+import {ActivatingAreaAroundContainer} from './activatingAreaAroundContainer';
+import './table.css';
 
 const CSS = {
-    inputField: "tcm-editable-table__input-field",
-    cell: "tcm-editable-table__cell",
-    selected: "tcm-editable-table__cell--focus",
-    table: "tcm-editable-table",
-    wrapper: "tcm-editable-table__wrapper"
+  inputField: 'tcm-editable-table__input-field',
+  cell: 'tcm-editable-table__cell',
+  selected: 'tcm-editable-table__cell--focus',
+  table: 'tcm-editable-table',
+  wrapper: 'tcm-editable-table__wrapper'
 };
 
 /**
  * Generates and manages _table contents.
  */
 export class Table {
+  /**
+   * Creates
+   */
+  constructor() {
+    this._numberOfColumns = 0;
+    this._numberOfRows = 0;
+    this._table = this._createTableWrapper();
+  }
 
-    constructor() {
-        this._numberOfColumns = 0;
-        this._numberOfRows = 0;
-        this._table = this._createTableWrapper();
+  /**
+   * Add column in table on index place
+   * @param {number} index
+   */
+  addColumn(index = Infinity) {
+    this._numberOfColumns++;
+    // Add cell in each row
+    for (let i = 0; i < this._table.children.length; i++) {
+      const cell = this._createClearCell();
+      this._addChildToElem(this._table.children[i], index, cell);
     }
+  };
 
-    addColumn(index = Infinity) {
-        this._numberOfColumns++;
-        // Add cell in each row
-        for (let i = 0; i < this._table.children.length; i++) {
-            const cell = this._createClearCell();
-            this._addChildToElem(this._table.children[i], index, cell);
-        }
-    };
+  /**
+   * Add row in table on index place
+   * @param {number} index
+   */
+  addRow(pos = Infinity) {
+    this._numberOfRows++;
+    const row = this._createClearRow();
+    this._addChildToElem(this._table, pos, row);
+    return row;
+  };
 
-    addRow(pos = Infinity) {
-        this._numberOfRows++;
-        const row = this._createClearRow();
-        this._addChildToElem(this._table, pos, row);
-        return row;
-    };
+  /**
+   * get html element of table
+   * @return {HTMLElement}
+   */
+  get htmlElement() {
+    return this._table.parentElement;
+  }
 
-    get htmlElement() {
-        return this._table.parentElement;
+  /**
+   * returns selected/edited sell
+   * @return {HTMLElement}
+   */
+  get selectedSell() {
+    return this._selectedCell;
+  }
+
+  /**
+   * Creates table structure
+   * @return {HTMLElement} tbody - where rows will be
+   * @private
+   */
+  _createTableWrapper() {
+    let table = createDOMElement('table', [CSS.table], null, [createDOMElement('tbody')]);
+    return table.firstElementChild;
+  }
+
+  /**
+   * Create editable area of cell
+   * @param {HTMLElement} cell - cell for which area is created
+   * @return {HTMLElement} - the area
+   * @private
+   */
+  _createContenteditableArea(cell) {
+    const div = createDOMElement('div', [CSS.inputField], {contenteditable: 'true'});
+    div.addEventListener('focus', () => {
+      cell.classList.add(CSS.selected);
+      this._selectedCell = cell;
+    });
+    div.addEventListener('blur', () => {
+      cell.classList.remove(CSS.selected);
+      this._selectedCell = null;
+    });
+    return div;
+  }
+
+  /**
+   * Creates clear cell
+   * @return {HTMLElement} - the cell
+   * @private
+   */
+  _createClearCell() {
+    const cell = createDOMElement('td', [CSS.cell]);
+    const content = this._createContenteditableArea(cell);
+    const area = this._createActivatingConteiner(content);
+
+    cell.appendChild(area);
+    cell.addEventListener('click', () => {
+      // Get to the edited part of the cell
+      content.focus();
+    });
+    return cell;
+  }
+
+  /**
+   * Create container int cell, where mouse will be detected
+   * @param {HTMLElement} content - the container content
+   * @return {HTMLElement} - the container
+   * @private
+   */
+  _createActivatingConteiner(content) {
+    const area = (new ActivatingAreaAroundContainer(content)).htmlElement;
+    area.classList.add(CSS.wrapper);
+    return area;
+  }
+
+  /**
+   * Add child element to elem's children on index place
+   * @param {HTMLElement} elem - container for child
+   * @param {number} index - place where child will be
+   * @param child - element for insert
+   * @private
+   */
+  _addChildToElem(elem, index, child) {
+    // if index is bigger than length of array then add in end
+    const indexToInsert = (index >= elem.children.length) ? null : elem.children[index];
+    elem.insertBefore(child, indexToInsert);
+  }
+
+  /**
+   * creates clear row
+   * @return {HTMLElement} the row
+   * @private
+   */
+  _createClearRow() {
+    const str = createDOMElement('tr');
+    for (let i = 0; i < this._numberOfColumns; i++) {
+      str.appendChild(this._createClearCell());
     }
-
-    get selectedSell() {
-        return this._selectedCell;
-    }
-
-    _createTableWrapper() {
-        let table = createDOMElement("table", [CSS.table], null, [createDOMElement("tbody")]);
-        return table.firstElementChild;
-    }
-
-    _createContenteditableArea(cell) {
-        const div = createDOMElement("div", [CSS.inputField], {contenteditable: "true"});
-        div.addEventListener("focus", () => {
-            cell.classList.add(CSS.selected);
-            this._selectedCell = cell;
-        });
-        div.addEventListener("blur", () => {
-            cell.classList.remove(CSS.selected);
-            this._selectedCell = null;
-        });
-        return div;
-    }
-
-    _createClearCell() {
-        const cell = createDOMElement("td", [CSS.cell]);
-        const content = this._createContenteditableArea(cell);
-        const area = this._createActivatingConteiner(content);
-        cell.appendChild(area);
-        cell.addEventListener("click", () => {
-            // Get to the edited part of the cell
-            content.focus();
-        });
-        return cell;
-    }
-
-    _createActivatingConteiner(content) {
-        const area = (new ActivatingAreaAroundContainer(content)).htmlElement;
-        area.classList.add(CSS.wrapper);
-        return area;
-    }
-
-    _addChildToElem(elem, index, child) {
-        // if index is bigger than length of array then add in end
-        const indexToInsert = (index >= elem.children.length) ? null : elem.children[index];
-        elem.insertBefore(child, indexToInsert);
-    }
-
-    _createClearRow() {
-        const str = createDOMElement("tr");
-        for (let i = 0; i < this._numberOfColumns; i++) {
-            str.appendChild(this._createClearCell());
-        }
-        return str;
-    }
+    return str;
+  }
 }
