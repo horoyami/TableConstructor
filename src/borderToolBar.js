c3722c30a6f5558e2b1b24061d536f5696d4080c
Vinogradov Victor <re.horoyami.not@gmail.com>
2018-10-28 14:12:09 +0300

    Lint and JSDoc: borderToolBar

--- src/borderToolBar.js
+++ src/borderToolBar.js
@@ -1,127 +1,146 @@
-import "./borderToolBar.css";
-import svgPlusButton from "./img/plus.svg";
-import {createDOMElement} from "./documentUtils";
+import './borderToolBar.css';
+import svgPlusButton from './img/plus.svg';
+import {createDOMElement} from './documentUtils';
 
 const CSS = {
-    highlightingLine: "tcm-border-menu",
-    hidden: "tcm-border-menu--hidden",
-    horizontalToolBar: "tcm-border-menu--horizontal",
-    horizontalHighlightingLine: "tcm-border-menu__1pxline--horizontal",
-    verticalToolBar: "tcm-border-menu--vertical",
-    verticalHighlightingLine: "tcm-border-menu__1pxline--vertical",
-    plusButton: "tcm-border-menu__add-button"
+  highlightingLine: 'tcm-border-menu',
+  hidden: 'tcm-border-menu--hidden',
+  horizontalToolBar: 'tcm-border-menu--horizontal',
+  horizontalHighlightingLine: 'tcm-border-menu__1pxline--horizontal',
+  verticalToolBar: 'tcm-border-menu--vertical',
+  verticalHighlightingLine: 'tcm-border-menu__1pxline--vertical',
+  plusButton: 'tcm-border-menu__add-button'
 };
 
 /**
  * An item with a menu that appears when you hover over a _table border
  */
 class BorderToolBar {
+  /**
+   * @param additionalStyles - additional styles for custom items
+   * @constructor
+   */
+  constructor(additionalStyles) {
+    this._additionalStyles = additionalStyles;
+    this._plusButton = this._generatePlusButton();
+    this._highlightingLine = this._generateHighlightingLineCoveringBorder();
+    this._toolBar = this._generateToolBar([this._plusButton, this._highlightingLine]);
+  }
 
-    /**
-     * @param additionalStyles - additional styles for custom items
-     * @constructor
-     */
-    constructor(additionalStyles) {
-        this._additionalStyles = additionalStyles;
-        this._plusButton = this._generatePlusButton();
-        this._highlightingLine = this._generateHighlightingLineCoveringBorder();
-        this._toolBar = this._generateToolBar([this._plusButton, this._highlightingLine]);
-    }
+  /**
+   * Make the entire item invisible
+   */
+  hide() {
+    this._toolBar.classList.add(CSS.hidden);
+  }
 
-    /**
-     * Make the entire item invisible
-     */
-    hide() {
-        this._toolBar.classList.add(CSS.hidden);
-    }
+  /**
+   * Make the entire item visible
+   */
+  show() {
+    this._toolBar.classList.remove(CSS.hidden);
+    this._highlightingLine.classList.remove(CSS.hidden);
+  };
 
-    /**
-     * Make the entire item visible
-     */
-    show() {
-        this._toolBar.classList.remove(CSS.hidden);
-        this._highlightingLine.classList.remove(CSS.hidden);
-    };
+  /**
+   * Hide only highlightingLine
+   */
+  hideLine() {
+    this._highlightingLine.classList.add(CSS.hidden);
+  };
 
-    /**
-     * Hide only highlightingLine
-     */
-    hideLine() {
-        this._highlightingLine.classList.add(CSS.hidden);
-    };
+  /**
+   * returns HTMLElement for insert in DOM
+   * @returns {HTMLElement}
+   */
+  get htmlElement() {
+    return this._toolBar;
+  }
 
-    /**
-     * returns HTMLElement for insert in DOM
-     * @returns {HTMLElement}
-     */
-    get htmlElement() {
-        return this._toolBar;
-    }
+  /**
+   * Generates a menu button to add rows and columns.
+   * @return {HTMLElement}
+   */
+  _generatePlusButton() {
+    const button = createDOMElement('div');
 
-    /**
-     * Generates a menu button to add rows and columns.
-     * @return {HTMLElement}
-     */
-    _generatePlusButton() {
-        const button = createDOMElement("div");
-        button.innerHTML = svgPlusButton;
-        button.classList.add(CSS.plusButton);
-        button.firstChild.addEventListener("click", (event) => {
-            event.stopPropagation();
-            const e = new CustomEvent('click', {'detail': {"x": event.pageX, "y": event.pageY}, 'bubbles': true});
-            button.dispatchEvent(e);
-        });
-        return button;
-    }
+    button.innerHTML = svgPlusButton;
+    button.classList.add(CSS.plusButton);
+    button.firstChild.addEventListener('click', (event) => {
+      event.stopPropagation();
+      const e = new CustomEvent('click', {'detail': {'x': event.pageX, 'y': event.pageY}, 'bubbles': true});
 
-    /**
-     * Generates line which сover border of _table
-     * @private
-     */
-    _generateHighlightingLineCoveringBorder(style) {
-        const border = createDOMElement("div", [CSS.highlightingLine, this._additionalStyles.highlightingLine]);
-        return border;
-    }
+      button.dispatchEvent(e);
+    });
+    return button;
+  }
 
-    /**
-     * Generates the main component of the class
-     * @param {array} children - child elements of toolbar
-     * @private
-     */
-    _generateToolBar(children) {
-        const toolBar = createDOMElement("div", [this._additionalStyles.toolBar, CSS.hidden], null, children);
-        return toolBar;
-    }
+  /**
+   * Generates line which сover border of _table
+   * @private
+   */
+  _generateHighlightingLineCoveringBorder(style) {
+    return createDOMElement('div', [CSS.highlightingLine, this._additionalStyles.highlightingLine]);
+  }
+
+  /**
+   * Generates the main component of the class
+   * @param {array} children - child elements of toolbar
+   * @private
+   */
+  _generateToolBar(children) {
+    return createDOMElement('div', [this._additionalStyles.toolBar, CSS.hidden], null, children);
+  }
 }
 
+/**
+ * An item with a menu that appears when you hover over a _table border horizontally
+ */
 export class HorizontalBorderToolBar extends BorderToolBar {
+  /**
+   * Creates
+   */
+  constructor() {
+    super({
+      highlightingLine: CSS.horizontalHighlightingLine,
+      toolBar: CSS.horizontalToolBar
+    });
+  }
 
-    constructor() {
-        super({
-            highlightingLine: CSS.horizontalHighlightingLine,
-            toolBar: CSS.horizontalToolBar
-        });
-    }
+  /**
+   * Move ToolBar to y coord
+   * @param {number} y - coord
+   */
+  showIn(y) {
+    const halfHeight = Math.floor(Number.parseInt(getComputedStyle(this._toolBar).height) / 2);
 
-    showIn(y) {
-        const halfHeight = Math.floor(Number.parseInt(getComputedStyle(this._toolBar).height) / 2);
-        this._toolBar.style.top = (y - halfHeight) + "px";
-        this.show();
-    }
+    this._toolBar.style.top = (y - halfHeight) + 'px';
+    this.show();
+  }
 }
 
+/**
+ * An item with a menu that appears when you hover over a _table border vertically
+ */
 export class VerticalBorderToolBar extends BorderToolBar {
+  /**
+   * Creates
+   */
+  constructor() {
+    super({
+      highlightingLine: CSS.verticalHighlightingLine,
+      toolBar: CSS.verticalToolBar
+    });
+  }
 
-    constructor() {
-        super({
-            highlightingLine: CSS.verticalHighlightingLine,
-            toolBar: CSS.verticalToolBar
-        });
-    }
+  /**
+   * Move ToolBar to x coord
+   * @param {number} x - coord
+   */
+  showIn(x) {
+    const halfWidth = Math.floor(Number.parseInt(getComputedStyle(this._toolBar).width) / 2);
 
-    showIn(x) {
-        let halfWidth = Math.floor(Number.parseInt(getComputedStyle(this._toolBar).width) / 2);
-        this._toolBar.style.left = (x - halfWidth) + "px";
-        this.show();
-    }
+    this._toolBar.style.left = (x - halfWidth) + 'px';
+    this.show();
+  }
 }
