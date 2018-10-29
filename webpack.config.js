b95ec80788b5e8811933e6fc6ecda3a1f2f0f0e7
Vinogradov Victor <re.horoyami.not@gmail.com>
2018-10-29 19:55:22 +0300

    postcss was pluged

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,33 @@
 const path = require("path");
+const postcssNesting = require("postcss-nesting");
 
 module.exports = {
-    entry: "./src/tableConstructor.js",
-    output: {
-        filename: "tableConstructor.js",
-        path: __dirname,
-        library: "TableConstructor",
-        libraryTarget: 'umd'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.svg$/,
-                loader: "svg-inline-loader"
+  entry: "./src/tableConstructor.js",
+  output: {
+    filename: "tableConstructor.js",
+    path: __dirname,
+    library: "TableConstructor",
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader",
+          {loader: 'css-loader', options: {importLoaders: 1}},
+          {
+            loader: 'postcss-loader', options: {
+              ident: 'postcss',
+              plugins: () => [
+                postcssNesting()
+              ]
             }
-        ]
-    }
+          }]
+      },
+      {
+        test: /\.svg$/,
+        loader: "svg-inline-loader"
+      }
+    ]
+  }
 };
