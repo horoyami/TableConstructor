64d55e8ffdd4d3c6bf015333e4b68fa3c9b328cb
Vinogradov Victor <re.horoyami.not@gmail.com>
2018-10-30 18:37:55 +0300

    DetectionAreas

--- src/containerWithDetectionAreas.js
+++ src/containerWithDetectionAreas.js
@@ -5,77 +5,39 @@ const CSS = {
   mainContainer: 'tcm-around-container',
   horizontalArea: 'tcm-around-container__horizontal-area',
   verticalArea: 'tcm-around-container__vertical-area',
-  rowContainer: 'tcm-around-container__row-container'
+  rowContainer: 'tcm-around-container__row-container',
+  top: 'tcm-around-container__horizontal-area--top',
+  bottom: 'tcm-around-container__horizontal-area--bottom',
+  left: 'tcm-around-container__vertical-area--left',
+  right: 'tcm-around-container__vertical-area--right',
 };
 
-/**
- * Container with area around content, where mouse detected
- */
-export class ContainerWithDetectionAreas {
-  /**
-   * Create the container
-   * @param {HTMLElement} content - container contents
-   * @param {boolean} isOutside - Determines which area border to use, internal to the container or external
-   * @constructor
-   */
-  constructor(content, isOutside = true) {
-    this._isOutside = isOutside;
-    this._content = content;
-    this._container = this._createMainContainer();
-  }
+export function addDetectionAreas(elem, isOutside) {
+  const topArea = createActivatingArea((this._isOutside ? 'top' : 'bottom'), [CSS.horizontalArea, CSS.top]);
+  const leftArea = createActivatingArea((this._isOutside ? 'left' : 'right'), [CSS.verticalArea, CSS.left]);
+  const rightArea = createActivatingArea((this._isOutside ? 'right' : 'left'), [CSS.verticalArea, CSS.right]);
+  const bottomArea = createActivatingArea((this._isOutside ? 'bottom' : 'top'), [CSS.horizontalArea, CSS.bottom]);
 
-  /**
-   * Returns html element of main container
-   * @return {HTMLElement}
-   */
-  get htmlElement() {
-    return this._container;
-  }
-
-  /**
-   * Creates html element of container
-   * @return {HTMLElement} - the container
-   * @private
-   */
-  _createMainContainer() {
-    return create('div', [ CSS.mainContainer ], null, [
-      this._createActivatingArea((this._isOutside ? 'top' : 'bottom'), CSS.horizontalArea),
-      this._createInlineAreaContent(),
-      this._createActivatingArea((this._isOutside ? 'bottom' : 'top'), CSS.horizontalArea)
-    ]);
-  }
-
-  /**
-   * Creates area that detects mouse enter
-   * @param {string} side - where area is (top, bottom, left, right)
-   * @param {object} style - additional styles
-   * @return {HTMLElement} - the area html element
-   * @private
-   */
-  _createActivatingArea(side, style) {
-    const area = create('div', [ style ]);
-
-    area.addEventListener('mouseenter', () => {
-      area.dispatchEvent(new CustomEvent('mouseInActivatingArea', {
-        'detail': {
-          'side': side
-        },
-        'bubbles': true
-      }));
-    });
-    return area;
-  }
-
-  /**
-   * Create vertical container with activating area in start and in end
-   * @return {HTMLElement} - the container
-   * @private
-   */
-  _createInlineAreaContent() {
-    return create('div', [ CSS.rowContainer ], null, [
-      this._createActivatingArea((this._isOutside ? 'left' : 'right'), CSS.verticalArea),
-      this._content,
-      this._createActivatingArea((this._isOutside ? 'right' : 'left'), CSS.verticalArea)
-    ]);
-  }
 }
+
+/**
+ * Creates area that detects mouse enter
+ * @param {string} side - where area is (top, bottom, left, right)
+ * @param {string[]} style - additional styles
+ * @return {HTMLElement} - the area html element
+ * @private
+ */
+function createActivatingArea(side, style) {
+  const area = create('div', style);
+
+  area.addEventListener('mouseenter', () => {
+    area.dispatchEvent(new CustomEvent('mouseInActivatingArea', {
+      'detail': {
+        'side': side
+      },
+      'bubbles': true
+    }));
+  });
+  return area;
+}
+
